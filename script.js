06b076a8b1c14fd87549f21d7c37d0cdbddf7dcc
Darlan Carvalho <darlan171201@gmail.com>
2022-08-26 12:14:21 -0300

    Fiz alterações no Javascript, o resultado não estava sendo escrito por conta de que não estava pegando o value do Id, por isso o Null.

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ let tarefaASeFazer = document.getElementById('input-tarefa');
 
 let enviar = document.getElementById('submit');
 
+let completar = document.getElementById('done');
+
 
 document.getElementById('submit').addEventListener('click', function () {
-    if (tarefaASeFazer != "") {
-        listaTarefas.innerHTML += `<li> ${tarefaASeFazer} </li>`
+    if (tarefaASeFazer.value == "") {
+        window.alert('Não podemos adicionar uma tarefa vazia, por favor digite alguma tarefa.')
     } else {
-        console.log('Não podemos adicionar uma tarefa vazia, por favor digite alguma tarefa.')
+        listaTarefas.innerHTML += `<li> ${tarefaASeFazer.value} </li>`
     }
 })
